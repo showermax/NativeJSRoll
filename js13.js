9bc0637afe7c2aaa6b420cae43971b734b7d9c82
showermax <sevenhauz@gmail.com>
2023-08-07 15:38:21 +0300

    bind

--- js13.js
+++ js13.js
@@ -1,16 +1,16 @@
-function a () {
-    console.log(this) // window - потому this только в момент вызова - это фанкшн деларейшн - для стрелочной в момент создания
+const Max = {
+    age: 28
 }
-a()
 
-const car = {
-    color: 'red',
-    showColor(){
-        console.log(this.color)
+const Dan = {
+    age: 1
+}
+
+const Person = {
+    age: 10,
+    getAge () {
+        console.log(this.age)
     }
 }
-car.showColor() // this ссылка на то, что слева от вызова функции (т.е. car)
 
-// initial try question dress nation wear raise tone spread plunge sun shoulder
-console.log(this)
-this.name='the name of the page'
+Person.getAge.bind(Dan).bind(Max)
